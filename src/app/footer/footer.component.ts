78fcd0aea48780cc926099e53c610bfff1dacbf5
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-07 17:09:55 +0100

    Cambiados selectores de componentes header y footer

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'footer',
+  selector: 'piepag',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.css']
 })

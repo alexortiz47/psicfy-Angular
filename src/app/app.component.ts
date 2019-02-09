5fece6c38c8f92520297fe528cc916c55dbdf171
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-09 13:28:46 +0100

    Recuperando por HTTP el objeto Psicologo en el servicio de Psicologo e inyectandolo en app.component.ts

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { PsicologosService } from './psicologos.service';
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,11 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
 
+  constructor(private psicologosService: PsicologosService) {
+    this.psicologosService.getAllPsicologos().then((res) => console.log(res))
+  }
+
+  ngOnInit() {
+
+  }
 }
--- src/app/psicologos.service.ts
+++ src/app/psicologos.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PsicologosService {
 
-  constructor() { }
+  constructor(private httpClient: HttpClient) {
+
+  }
+
+  getAllPsicologos() {
+    let url = 'https://proyecto-final-neoland-aob.firebaseio.com/psicologos.json';
+    return this.httpClient.get(url).toPromise()
+  }
 }

19816b96618583633e3fcb4427faeab6b7a767cc
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-10 21:08:04 +0100

    Modificacion para que el componente Error salga sin header y sin footer

--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'cabecera',
@@ -7,11 +8,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponent implements OnInit {
 
-  constructor() {
+  constructor(private router: Router) {
 
   }
 
   ngOnInit() {
+  }
+
+  irEvaluate() {
+    this.router.navigate(['evaluate'])
+  }
+
+  irCerca() {
+    this.router.navigate(['cerca'])
+  }
+
+  irRegistro() {
+    this.router.navigate(['registro'])
+  }
+
+  irLogin() {
+    this.router.navigate(['login'])
   }
 
 }

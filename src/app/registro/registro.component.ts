a1515ed120721e56f2202d0c92fe54cfbaef2ede
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-12 15:47:25 +0100

    Validaciones de repetir correo y contraseña

--- src/app/registro/registro.component.ts
+++ src/app/registro/registro.component.ts
@@ -36,10 +36,29 @@ export class RegistroComponent implements OnInit {
         Validators.required,
         Validators.pattern(/^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/)
       ]),
+      correo_repeat: new FormControl(''),
       password: new FormControl('', [
         Validators.pattern(/(?!^[0-9]*$)(?!^[a-zA-Z]*$)^([a-zA-Z0-9]{6,15})$/)
-      ])
-    })
+      ]),
+      password_repeat: new FormControl('')
+    }, [
+      this.repeatPasswordValidator,
+      this.repeatCorreoValidator
+    ])
+  }
+
+  repeatCorreoValidator(group: FormGroup) {
+    let correo = group.controls['correo'].value
+    let correo_repeat = group.controls['correo_repeat'].value
+
+    return (correo == correo_repeat) ? null : { 'correo_repeat': 'El correo no coincide' }
+  }
+
+  repeatPasswordValidator(group: FormGroup) {
+    let password = group.controls['password'].value
+    let password_repeat = group.controls['password_repeat'].value
+
+    return (password == password_repeat) ? null : { 'password_repeat': 'La contraseña no coincide' }
   }
 
   manejarRegistro() {

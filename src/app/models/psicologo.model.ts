ecbd7f43323b3ad0b0d622d24d1249e72cafd68b
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-12 17:42:26 +0100

    Retocado el modelo Psicologo, añadiendo img y especialidad

--- src/app/models/psicologo.model.ts
+++ src/app/models/psicologo.model.ts
@@ -3,13 +3,17 @@ export class Psicologo {
   id: number;
   nombre: string;
   apellidos: string;
-  numCol: number;
+  numColeg: number;
+  especialidad: string;
+  imgUrl: string;
 
-  constructor(pId: number, pNombre: string, pApellidos: string, pNumCol: number) {
+  constructor(pId: number, pNombre: string, pApellidos: string, pNumCol: number, pEspecialidad: string, pImgUrl: string) {
     this.id = pId;
     this.nombre = pNombre;
     this.apellidos = pApellidos;
-    this.numCol = pNumCol;
+    this.numColeg = pNumCol;
+    this.especialidad = pEspecialidad
+    this.imgUrl = pImgUrl;
   }
 
 }

2556c652f0f708cca42b25e7d9019c2ba3d5b324
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-25 23:31:31 +0100

    Maquetando el componente de preguntas (evaluate), sacando las preguntas a un componente distinto, falta obtener los datos del formulario

--- src/app/evaluate/evaluate.component.ts
+++ src/app/evaluate/evaluate.component.ts
@@ -7,9 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EvaluateComponent implements OnInit {
 
-  constructor() { }
+  mensaje: boolean;
+  inicio: boolean;
+
+  constructor() {
+    this.mensaje = true;
+    this.inicio = false;
+  }
 
   ngOnInit() {
+  }
+
+  iniciarPreguntas() {
+    if(!this.inicio || this.mensaje) {
+      this.inicio = true;
+      this.mensaje = false;
+    }
+  }
+
+  volverMensajeInicio(){
+    if(this.inicio || !this.mensaje) {
+      this.inicio = false;
+      this.mensaje = true;
+    }
   }
 
 }

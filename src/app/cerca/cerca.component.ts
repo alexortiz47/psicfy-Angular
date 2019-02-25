c50a36678799fc4831c2a25e084f370e97ead818
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-02-25 23:27:42 +0100

    Cambiado el checkbox a un select en el filtrado de psicólogos para especialidades y poblacion

--- src/app/cerca/cerca.component.ts
+++ src/app/cerca/cerca.component.ts
@@ -11,20 +11,22 @@ export class CercaComponent implements OnInit {
 
   arrPsico: Psicologo[]
 
-  arrEspecialidades: string[];
-  arrPoblaciones: string[];
-
   constructor(private psicologosService: PsicologosService) {
     this.psicologosService.getAllPsicologos().then((res) => {
       console.log(res)
       this.arrPsico = res;
     })
-    this.arrEspecialidades = ['Ansiedad', 'Depresión', 'Trastornos del sueño', 'Trastornos alimenticios', 'Pareja y sexualidad', 'Familia', 'Consumo de tóxicos', 'Adicciones', 'Duelo', 'Trastorno por estrés postraumático', 'Violencia de género', 'Discapacidad', 'Trastorno mental grave', 'Coaching'];
-    this.arrPoblaciones = ['Infanto-Juvenil', 'Adultos'];
-
   }
 
   ngOnInit() {
+  }
+
+  seleccionarPob($event) {
+    console.log($event.target.value);
+  }
+
+  seleccionarEsp($event) {
+    console.log($event.target.value);
   }
 
 }

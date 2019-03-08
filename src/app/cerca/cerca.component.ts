05acc678dfec161de63877ee1b72a8435e5e98b0
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-08 13:02:34 +0100

    Componente cerca a falta de añadir el filtrado

--- src/app/cerca/cerca.component.ts
+++ src/app/cerca/cerca.component.ts
@@ -12,11 +12,31 @@ import { Especialidad } from '../models/especialidad.model';
 export class CercaComponent implements OnInit {
 
   arrPsico: Psicologo[]
-  arrEsp: Especialidad[]
+  arrEsp: Especialidad[] // Para pintar el select de especialidades
+  arrDatosPsico: any[]
 
   constructor(private psicologosService: PsicologosService, private especialidadesService: EspecialidadesService) {
+    this.arrDatosPsico = []
     this.psicologosService.getAllPsicologos().then((res) => {
+      // console.log(res)
       this.arrPsico = res;
+      this.arrPsico.map(psico => {
+        this.especialidadesService.getEspByPsicologo(psico.id).then(res => {
+          // console.log(res)
+          let datosPsico = {
+            nombre: psico.nombre,
+            apellidos: psico.apellidos,
+            numColeg: psico.numColeg,
+            especialidades: res,
+            poblacion: psico.poblacion,
+            domicilio: psico.domicilio,
+            codPostal: psico.codPostal,
+            correo: psico.correo
+          }
+          this.arrDatosPsico.push(datosPsico)
+        })
+      })
+      console.log(this.arrDatosPsico)
     })
     this.especialidadesService.getAllEspecialidades().then(res => {
       this.arrEsp = res

6f2aa9f205a0b13aeaeb1efdaa8a681facbd5b4a
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-11 12:22:59 +0100

    Evaluacion de las preguntas, falta meter los resultados bien redactados

--- src/app/evaluate/evaluate.component.ts
+++ src/app/evaluate/evaluate.component.ts
@@ -16,6 +16,18 @@ export class EvaluateComponent implements OnInit {
   arrPreguntas: Pregunta[]
   arrRespuestas: any[]
   edad: number
+  totalDepresion: number
+  totalAnsiedad: number
+  totalAdiccion: number
+  totalEmocion: number
+  depresion1: boolean
+  depresion2: boolean
+  ansiedad1: boolean
+  ansiedad2: boolean
+  adicciones1: boolean
+  adicciones2: boolean
+  emociones1: boolean
+  emociones2: boolean
 
   constructor(private preguntasService: PreguntasService, private router: Router) {
     this.preguntasService.getAllPreguntas().then((res) => {
@@ -25,6 +37,18 @@ export class EvaluateComponent implements OnInit {
     this.preguntas = false
     this.resultado = false
     this.arrRespuestas = []
+    this.totalDepresion = 0
+    this.totalAnsiedad = 0
+    this.totalAdiccion = 0
+    this.totalEmocion = 0
+    this.depresion1 = false
+    this.depresion2 = false
+    this.ansiedad1 = false
+    this.ansiedad2 = false
+    this.adicciones1 = false
+    this.adicciones2 = false
+    this.emociones1 = false
+    this.emociones2 = false
   }
 
   ngOnInit() {
@@ -59,6 +83,48 @@ export class EvaluateComponent implements OnInit {
   enviarRespuestas() {
     console.log(this.edad)
     console.log(this.arrRespuestas)
+
+    for(let i = 0; i <= 4; i++){
+      this.totalDepresion += parseInt(this.arrRespuestas[i].value)
+    }
+    for(let i = 5; i <= 9; i++){
+      this.totalAnsiedad += parseInt(this.arrRespuestas[i].value)
+    }
+    for(let i = 10; i <= 14; i++){
+      this.totalAdiccion += parseInt(this.arrRespuestas[i].value)
+    }
+    for(let i = 15; i <= 19; i++){
+      this.totalEmocion += parseInt(this.arrRespuestas[i].value)
+    }
+    console.log(`Depresion: ${this.totalDepresion}`)
+    console.log(`Ansiedad: ${this.totalAnsiedad}`)
+    console.log(`Adicciones: ${this.totalAdiccion}`)
+    console.log(`Emociones: ${this.totalEmocion}`)
+
+    if(this.totalDepresion > 13 && this.totalDepresion < 19) {
+      this.depresion1 = true
+    } else if (this.totalDepresion > 18) {
+      this.depresion2 = true
+    }
+
+    if(this.totalAnsiedad > 13 && this.totalAnsiedad < 19) {
+      this.ansiedad1 = true
+    } else if (this.totalAnsiedad > 18) {
+      this.ansiedad2 = true
+    }
+
+    if(this.totalAdiccion > 13 && this.totalAdiccion < 19) {
+      this.adicciones1 = true
+    } else if (this.totalAdiccion > 18) {
+      this.adicciones2 = true
+    }
+
+    if(this.totalEmocion > 13 && this.totalEmocion < 19) {
+      this.emociones1 = true
+    } else if (this.totalEmocion > 18) {
+      this.emociones2 = true
+    }
+
     this.resultado = true
     this.preguntas = false
   }

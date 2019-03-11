a3e3d76084a1fe1a0caef10ce0d279b74f9f3380
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-11 15:31:33 +0100

    Acabado el filtrado de los psicólogos

--- src/app/cerca/cerca.component.ts
+++ src/app/cerca/cerca.component.ts
@@ -1,23 +1,30 @@
-import { Component, OnInit } from '@angular/core';
-import { PsicologosService } from '../psicologos.service';
-import { Psicologo } from '../models/psicologo.model';
-import { EspecialidadesService } from '../especialidades.service';
-import { Especialidad } from '../models/especialidad.model';
+import { Component, OnInit } from "@angular/core";
+import { PsicologosService } from "../psicologos.service";
+import { Psicologo } from "../models/psicologo.model";
+import { EspecialidadesService } from "../especialidades.service";
+import { Especialidad } from "../models/especialidad.model";
+import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 
 @Component({
-  selector: 'cerca',
-  templateUrl: './cerca.component.html',
-  styleUrls: ['./cerca.component.css']
+  selector: "cerca",
+  templateUrl: "./cerca.component.html",
+  styleUrls: ["./cerca.component.css"]
 })
 export class CercaComponent implements OnInit {
+  arrPsico: Psicologo[];
+  arrEsp: Especialidad[]; // Para pintar el select de especialidades
+  arrDatosPsico: any[];
+  arrFiltrado: any[];
+  poblacion: string;
+  especialidad: string;
 
-  arrPsico: Psicologo[]
-  arrEsp: Especialidad[] // Para pintar el select de especialidades
-  arrDatosPsico: any[]
+  constructor(
+    private psicologosService: PsicologosService,
+    private especialidadesService: EspecialidadesService
+  ) {
+    this.arrDatosPsico = [];
 
-  constructor(private psicologosService: PsicologosService, private especialidadesService: EspecialidadesService) {
-    this.arrDatosPsico = []
-    this.psicologosService.getAllPsicologos().then((res) => {
+    this.psicologosService.getAllPsicologos().then(res => {
       // console.log(res)
       this.arrPsico = res;
       this.arrPsico.map(psico => {
@@ -32,26 +39,38 @@ export class CercaComponent implements OnInit {
             domicilio: psico.domicilio,
             codPostal: psico.codPostal,
             correo: psico.correo
-          }
-          this.arrDatosPsico.push(datosPsico)
-        })
-      })
-      console.log(this.arrDatosPsico)
-    })
+          };
+          this.arrDatosPsico.push(datosPsico);
+        });
+      });
+      this.arrFiltrado = this.arrDatosPsico
+    });
     this.especialidadesService.getAllEspecialidades().then(res => {
-      this.arrEsp = res
-    })
+      this.arrEsp = res;
+    });
+    this.poblacion = "Todos";
+    this.especialidad = "Todas";
+    this.arrFiltrado = this.arrDatosPsico;
   }
 
-  ngOnInit() {
-  }
+  ngOnInit() {}
 
-  seleccionarPob($event) {
-    console.log($event.target.value);
-  }
+  seleccion($event) {
+    // console.log(this.poblacion);
+    // console.log(this.especialidad);
 
-  seleccionarEsp($event) {
-    console.log($event.target.value);
-  }
+    this.arrFiltrado = [...this.arrDatosPsico]; // Cada vez que se hace change de alguno de los select, genera un nuevo array, y sobre ese evalua, si el de la poblacion es 'todos' no entra en el primer if, al igual que si el de las especialidades es 'todas'
 
+    if (this.poblacion != "Todos") {
+      this.arrFiltrado = this.arrFiltrado.filter(psico => {
+        return psico.poblacion.split(",").includes(this.poblacion);
+      });
+    }
+
+    if (this.especialidad != "Todas") {
+      this.arrFiltrado = this.arrFiltrado.filter(psico => {
+        return psico.especialidades.map(item => item.nombre).includes(this.especialidad);
+      });
+    }
+  }
 }

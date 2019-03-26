0152b2fb137ab2a78a9128ff82ff409995be79dd
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-26 16:17:46 +0100

    instalado lo necesario para subir web

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBtE868Oaw8vveHOl6apunnGXL9HkvfXUY",
+    authDomain: "proyecto-final-neoland-aob.firebaseapp.com",
+    databaseURL: "https://proyecto-final-neoland-aob.firebaseio.com",
+    projectId: "proyecto-final-neoland-aob",
+    storageBucket: "proyecto-final-neoland-aob.appspot.com",
+    messagingSenderId: "89259251185"
+  }
 };

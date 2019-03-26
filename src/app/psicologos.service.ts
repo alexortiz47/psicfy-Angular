b6694752b5bb0e3a267f53fd7d2a32cad176bdd0
Alejandro Ortiz <ortizalex.47@gmail.com>
2019-03-26 19:01:26 +0100

    cambios a https funcionando, fallo autorecarga arreglado

--- src/app/psicologos.service.ts
+++ src/app/psicologos.service.ts
@@ -11,7 +11,7 @@ export class PsicologosService {
 
   constructor(private httpClient: HttpClient) {
     // this.url = 'http://localhost:3000/psicologos';
-    this.url = 'http://back.psicfy.com/psicologos';
+    this.url = 'https://back.psicfy.com/psicologos';
   }
 
   doRegistro(value) {
